9cf948831dd492fab45811def9654fb24a3c8fe8
uilmi <uilmi@na.edu>
2022-01-26 17:11:47 +0700

    Adding Newsletter & Contact Page

--- components/user/UpdateProfile.js
+++ components/user/UpdateProfile.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import { UPDATE_PROFILE_RESET } from '../../redux/constants/userConstants';
